a146ce39046b67e0fe160b5877799179a78f8dfa
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-03-06 21:03:52 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
-import urllib, json, random, re
+import json
+import random
+import re
+import urllib
 from urllib.request import Request, urlopen
 
-from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_handler
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
 
-class TronaldDump(MycroftSkill):
+
+class TronaldDump(OVOSSkill):
 
     @intent_handler('dump.tronald.intent')
     def handle_dump_tronald(self, message):
         topic = message.data.get('topic', '')
-        url_params = urllib.parse.urlencode({ 'query': topic })
+        url_params = urllib.parse.urlencode({'query': topic})
         api_url = 'https://api.tronalddump.io/search/quote?%s' % url_params
         headers = {'User-Agent': 'Mozilla/5.0'}
         try:
@@ -27,8 +31,6 @@ class TronaldDump(MycroftSkill):
         except urllib.error.URLError as e:
             self.speak_dialog('url.error')
 
-def create_skill():
-    return TronaldDump()
 
 def remove_http_links(raw_quote):
     return re.sub(r'https?:\/\/.*\s?', '', raw_quote)
